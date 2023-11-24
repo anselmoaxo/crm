4b24244bc471cc693d19e5d2f57b4f15bfbeacb0
Anselmo <anselmo.cstecnologia@gmail.com>
2023-11-24 17:36:52 -0300

    paginacao finalizada e campod e busca

--- customer/urls.py
+++ customer/urls.py
@@ -1,7 +1,7 @@
 
 
 from django.urls import path
-from .views import ListCustomer, CreateCustomer, UpdateCustomer
+from .views import ListCustomer, CreateCustomer, UpdateCustomer, DeleteCustomer
 
 
 app_name = "customer"
@@ -10,5 +10,6 @@ urlpatterns = [
 
     path('list/', ListCustomer.as_view(), name='list'),
     path('create/', CreateCustomer.as_view(), name='create'),
-    path('<int:id>/', UpdateCustomer.as_view(), name='update')
+    path('<int:id>/', UpdateCustomer.as_view(), name='update'),
+    path('<int:id>/delete/', DeleteCustomer.as_view(), name='delete'),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,18 +1,32 @@
-from typing import Any
+
 from django.db import models
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView,UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Customer
 from .forms import CustomerForm
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 # Create your views here.
 class ListCustomer(ListView):
     template_name = "customer/customer_list.html"
+    paginate_by = 10
     model = Customer
-    queryset = Customer.objects.all()
     context_object_name = 'customers'
 
+    def get_queryset(self):
+        retorno_pesquisa = self.request.GET.get('pesquisa_name')
+        if retorno_pesquisa:
+            customers = self.model.objects.filter(
+               Q(first_name__icontains=retorno_pesquisa) |
+               Q(last_name__icontains=retorno_pesquisa)
+               )
+        else:
+            customers = self.model.objects.all()
+
+        return customers
+    
 
 class CreateCustomer(CreateView):
     template_name = "customer/customer_create.html"
@@ -41,4 +55,11 @@ class UpdateCustomer(UpdateView):
         return reverse('customer:list')
     
     
+class DeleteCustomer(DeleteView):
+    def get_object(self):
+        id = self.kwargs.get('id')
+        return get_object_or_404(Customer, id=id)
+    
+    def get_success_url(self):
+        return reverse('customer:list')
 
